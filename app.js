53fc526e09c4c558e262697ee0dc911a7d910d0c
Viktor Skyba <skyba.viktor2016@gmail.com>
2026-02-16 22:53:04 +0200

    feat: add styles; add functionality

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('keyecho');
+
+
+function changeDisplayMode() {
+    document.querySelector('.key-pressed').style.display = 'flex';
+    document.querySelector('.empty-state-msg').style.display = 'none';
+    showMsg = false;
+}
+
+showMsg = true;
+document.addEventListener('keydown', (e) => {
+    if (showMsg) changeDisplayMode();
+    document.querySelector('.key-name').textContent = `${e.key}`;
+    document.querySelector('.key-code').textContent = `${e.code}`;
+    document.querySelector('.key-history-text').textContent += `${e.key}`;
+})
